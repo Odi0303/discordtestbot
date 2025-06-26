addbfbf167d98ad72fab992bf447ee161ca88975
Odi0303 <berbervurucu@gmail.com>
2025-06-26 09:08:57 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.once('ready', () => {
+  console.log(`Bot aktif: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
